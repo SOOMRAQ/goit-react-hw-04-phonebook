15f1fe80a9d6fd53d4674d9506ed7d76788b499c
Nikita Taranenko <soomraq.x@gmail.com>
2022-11-28 11:31:27 +0200

    refactoring with hooks

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { StyledModalBackdrop, StyledModalContent } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  });
+
+  const handleKeyDown = ({ code }) => {
+    code === 'Escape' && this.props.onClose();
+  };
+
+  const handleBackdropClick = ({ target, currentTarget }) => {
+    currentTarget === target && onClose();
+  };
+
+  return createPortal(
+    <StyledModalBackdrop onClick={handleBackdropClick}>
+      <StyledModalContent>{children}</StyledModalContent>
+    </StyledModalBackdrop>,
+    modalRoot
+  );
+};
+
+export default Modal;
